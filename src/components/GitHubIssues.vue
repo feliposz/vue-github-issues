f2d46ec294cd01e4d1d55ef4d6000e5c9dd0c78a
feliposz <feliposz@gmail.com>
2019-02-03 20:46:57 -0200

    issues details

--- src/components/GitHubIssues.vue
+++ src/components/GitHubIssues.vue
@@ -2,9 +2,13 @@
   <b-container>
     <b-row>
       <b-col md="12">
+
         <h1>Vue.js + GitHub Issues</h1>
+
         <hr>
+
         <p class="lead">List issues from a Github repository using Vue.</p>
+
         <b-form inline @submit="onSubmit" @reset="onReset">
           <label for="username" class="sr-only">Username</label>
           <b-input id="username" class="mb-2 mr-sm-2 mb-sm-0" type="text" v-model="form.username" required placeholder="Username" />
@@ -13,11 +17,28 @@
           <b-button class="mb-2 mr-sm-2 mb-sm-0" type="submit" variant="success">Search</b-button>
           <b-button class="mb-2 mr-sm-2 mb-sm-0" type="reset" variant="danger">Reset</b-button>
         </b-form>
+
         <hr>
+
         <img src="/loading.svg" alt="Loading..." v-if="loader.getIssues">
-        <b-table bordered :items="issues" v-if="!loader.getIssues && issues.length > 0">
+
+        <b-table bordered :fields="fields" :items="issues" v-if="!loader.getIssues && issues.length > 0 && !selectedIssue.number">
+          <template slot="number" slot-scope="data">
+            <a @click.prevent.stop="getIssue(data.value)" href="">{{data.value}}</a>
+          </template>
+          <template slot="title" slot-scope="data">
+            {{data.value}}
+          </template>
         </b-table>
+
         <b-alert variant="warning" :show="!loader.getIssues && error.status === 'error'" v-text="error.message"></b-alert>
+
+        <template v-if="selectedIssue.number">
+          <h1>{{selectedIssue.title}}</h1>
+          <div>{{selectedIssue.body}}</div>
+          <b-button variant="primary" @click.prevent.stop="clearIssue">Back</b-button>
+        </template>
+
       </b-col>
     </b-row>
   </b-container>
@@ -35,7 +56,9 @@ export default {
         username: "",
         repository: ""
       },
+      fields: ["number", "title"],
       issues: [],
+      selectedIssue: {},
       error: {
         status: "",
         message: ""
@@ -49,23 +72,7 @@ export default {
   methods: {
     async onSubmit(evt) {
       evt.preventDefault();
-      try {
-        this.loader.getIssues = true;
-        const url = `https://api.github.com/repos/${this.form.username}/${
-          this.form.repository
-        }/issues`;
-        const result = await axios.get(url);
-        this.issues = result.data.map(issue => ({
-          number: issue.number,
-          title: issue.title
-        }));
-      } catch {
-        this.error.status = "error";
-        this.error.message = "Not found";
-        this.issues = [];
-      } finally {
-        this.loader.getIssues = false;
-      }
+      await this.getIssues();
     },
 
     onReset(evt) {
@@ -75,6 +82,44 @@ export default {
       this.issues = [];
       this.error.status = "";
       this.error.message = "";
+    },
+
+    async getIssues() {
+      try {
+        this.loader.getIssues = true;
+        const url = `https://api.github.com/repos/${this.form.username}/${
+          this.form.repository
+        }/issues`;
+        const result = await axios.get(url);
+        this.issues = result.data;
+      } catch {
+        this.error.status = "error";
+        this.error.message = "Not found";
+        this.issues = [];
+      } finally {
+        this.loader.getIssues = false;
+      }
+    },
+
+    async getIssue(issueNumber) {
+      try {
+        this.loader.getIssue = true;
+        const url = `https://api.github.com/repos/${this.form.username}/${
+          this.form.repository
+        }/issues/${issueNumber}`;
+        const result = await axios.get(url);
+        this.selectedIssue = result.data;
+      } catch {
+        this.error.status = "error";
+        this.error.message = "Issue not found";
+        this.selectedIssue = {};
+      } finally {
+        this.loader.getIssue = false;
+      }
+    },
+
+    clearIssue() {
+      this.selectedIssue = {};
     }
   }
 };
